032af21f60beae78144e8b3f8d1ab60f17ff83a6
ashok-kumar-032 <av6017819@gmail.com>
2023-05-03 14:52:12 +0530

    section

--- src/Components/Dog.jsx
+++ src/Components/Dog.jsx
@@ -7,16 +7,16 @@ const Dog = () => {
         <section className='bg_section_1 py-5'>
             <Container>
                 <Row className='p_50 text-center text-lg-start'>
-                    <Col lg={4}>
+                    <Col lg={5}>
                         <p className='fw_400 fs_45 ff_Slackey clr_dark_blue'>Facilities we
                             provides</p>
                     </Col>
-                    <Col lg={8} className='px-5'>
+                    <Col lg={7} className='px-5'>
                         <p className='fw_400 fs_16 ff_Poppins clr_grey mt-3'>
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim  velit esse cillum dolore eu fugiat nulla pariatur.</p>
                     </Col>
                 </Row>
-                <div className=' d-flex align-items-center  justify-content-center py-5'>
+                <div className=' d-flex align-items-center  justify-content-center py-5 mt-md-5'>
                     <div className='position-relative'><img className='dog border_dog  w_100 mt_50' src={dog} alt="" />
                         <span className='bol_r_1'></span>
                         <span className='bol_l_1'></span>
