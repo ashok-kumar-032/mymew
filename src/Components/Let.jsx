a1207df28ddec161c9fba04f040348ff74860c1d
sukhvinder-jaat <monudhaka829@gmail.com>
2023-05-04 10:08:33 +0530

    aninmation

--- src/Components/Let.jsx
+++ src/Components/Let.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
 import coll from '../assets/coll.png'
 import let_2 from '../assets/let_2.png'
+import plus from '../assets/green_plus.png'
+import plus_red from '../assets/red_plus.png'
+import orange_line from '../assets/orange_line.png'
 
 const Let = () => {
     return (
         <section className='bg_section_1 py-5'>
-            <Container>
-                <Row>
+            <Container className=' pb-lg-5'>
+                <Row className='pb-5'>
                     <Col lg={6} className='mt-5'>
                         <p className='fw_400 fs_45 ff_Slackey clr_dark_blue text-center text-lg-start'>Lets book a call and stay connected </p>
                         <div className=' d-flex mt-4 flex-column flex-sm-row align-items-center'>
@@ -25,9 +28,13 @@ const Let = () => {
                             <p className='w_500 fs_18 ff_Poppins clr_dark_blue Call ms-sm-5 mt-2'><span><img src={coll} alt="" /></span> Emergency Call</p>
                         </div>
                     </Col>
-                    <Col lg={6} className='mt-4 mt-sm-0'>
-                        <img className=' w-100' src={let_2} alt="" />
-                    </Col></Row>
+                    <Col lg={6} className='mt-4 mt-sm-0 position-relative'>
+                        <img className=' w-100' src={let_2} />
+                        <img src={plus} className='green_plus position-absolute grow_up' />
+                        <img src={plus_red} className='plus_red position-absolute rotate_animation' />
+                        <img src={orange_line} className='orange_line position-absolute move_animation' />
+                    </Col>
+                </Row>
             </Container>
         </section>
     )
